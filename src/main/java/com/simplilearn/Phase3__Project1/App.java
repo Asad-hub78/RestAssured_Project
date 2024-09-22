644b7b4fbc9e85f9295dc4cc4ad0ce396244d8e4
Folder <marchal_001@yahoo.com>
2024-09-22 05:36:58 -1100

    First Commit

---
+++ src/main/java/com/simplilearn/Phase3__Project1/App.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Phase3__Project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
